8aa4a7badb553584e644927862e88c655a183a66
claude.wetienana <claude.wetienana@yahoo.com>
2017-10-20 23:31:46 -0400

    Add some feature

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171014083338) do
+ActiveRecord::Schema.define(version: 20171014094805) do
 
   create_table "students", force: true do |t|
     t.string   "name"
     t.string   "surname"
     t.date     "birthday"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "towns", force: true do |t|
+    t.string   "name"
+    t.float    "lat"
+    t.float    "lon"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
